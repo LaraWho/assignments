e7765f9a0cb85c20b5dbda40f277de7adc84e8ff
Lara Potjewyd <larapotjewyd@gmail.com>
2019-01-26 20:28:07 -0700

    almost finished the olympic practices

---
+++ exercises/if-statement-olympics/olympics.js
@@ -0,0 +1,145 @@
+////CONDITIONALS
+
+////PRELIMINARIES
+// if(5 > 3) {
+//   console.log("is greater than")
+// }
+
+// if("cat".length === 3) {
+//   console.log("is the length")
+// }
+
+// console.log("cat" === "dog" ? "the same" : "not the same")
+
+////BRONZE
+// var person = {
+//   name: "Bobby",
+//   age: 12
+// }
+// console.log(person.age >= 18 ? `${person.name} is allowed to go to the movie` : `${person.name} is NOT allowed to go to the movie`);
+
+// console.log(person["name"].startsWith("B") ? `${person.name} is allowed to go to the movie` : `${person.name} is NOT allowed to go to the movie`);
+
+// console.log(person.age >= 18 && person.name.startsWith("B") ? `${person.name} is allowed to go to the movie` : `${person.name} is NOT allowed to go to the movie`);
+
+
+////SILVER
+// if(1 === "1") {
+//   console.log("strict")
+// } else if(1 == "1") {
+//   console.log("loose")
+// } else {
+//   "not equal at all"
+// }
+
+// if((1 <= 2 && 2 === 4) || (3 * 4) > 10 && 5 + 10 > 10) {
+//   console.log("yes");
+// } 
+
+////GOLD 
+// if(typeof "dog" == "string") {
+//   console.log(typeof "dog");
+// }
+
+// if(typeof "true" == "boolean") {
+//   console.log("tis indeed a boolean!");
+// } else {
+//   console.log("nope")
+// }
+
+// console.log(typeof true == "boolean" ? "tis indeed a boolean!" : "nope");
+
+// var str = "s".charCodeAt();
+// var num = 12;
+
+// if(str > num) {
+//   console.log(`Yes: ${str} is bigger than ${num}`);
+// } else {
+//   console.log(`Nope: ${str} is NOT bigger than ${num}`);
+// }
+
+// var myNum = 10;
+// console.log(myNum % 2 === 0 ? "even" : "odd");
+
+  //// -------------------------
+
+////LOOPS
+
+////PRELIMINARIES
+// for(var i = 0; i < 10; i++) {
+//   console.log(i)
+// }
+
+// for(var j = 9; j >= 0; j--) {
+//   console.log(j)
+// }
+
+// var fruit = ["banana", "orange", "apple", "kiwi"]
+// for(var i = 0; i < fruit.length; i++) {
+//   console.log(fruit[i])
+// }
+
+////BRONZE
+// var nums = []
+// for(var i = 0; i < 10; i++) {
+//   nums.push(i)
+// }
+// console.log(nums)
+
+// for(var i = 0; i <= 100; i++) {
+//   if(i % 2 === 0) {
+//     console.log(i)
+//   }
+// }
+
+// var fruit = ["banana", "orange", "apple", "kiwi", "pear", "peach"];
+// var newArr = []
+
+// fruit.forEach((e,i) => {
+//   i % 2 === 1 ? newArr.push(e) : null
+// })
+// console.log(newArr)
+
+////SILVER
+// var peopleArray = [
+//   {
+//     name: "Harrison Ford",
+//     occupation: "Actor"
+//   },
+//   {
+//     name: "Justin Bieber",
+//     occupation: "Singer"
+//   },
+//   {
+//     name: "Vladimir Putin",
+//     occupation: "Politician"
+//   },
+//   {
+//     name: "Oprah",
+//     occupation: "Entertainer"
+//   }
+// ]
+// 1
+// peopleArray.forEach(person => {
+//   console.log(person.name)
+// })
+
+// var jobs = [];
+// var names = [];
+// peopleArray.forEach(person => {
+//   jobs.push(person.occupation)
+//   names.push(person.name)
+// })
+// console.log({jobs}, {names});
+
+// var someJobs = [];
+// var someNames = [];
+// peopleArray.forEach((e, i) => {
+//   i % 2 === 0 ? someNames.push(e.name) : someJobs.push(e.occupation)
+// })
+// console.log({someJobs}, {someNames})
+
+////GOLD
+
+
+
--- exercises/small-exercises/playground.js
+++ exercises/small-exercises/playground.js
@@ -1,7 +1,7 @@
 function sayHello(name, age) {
   console.log(`Hello, ${name}! You have lived ${age} years! Well done!`);
 }
-sayHello('Lara', 30);
+// sayHello('Lara', 30);
 
 var shopper = {
   name: 'Lara',
@@ -17,10 +17,55 @@ var shopper = {
    }
 }
 
-shopper.getStatus()
-console.log(shopper.groceryCart[1], 'is delicious')
+// shopper.getStatus()
+// console.log(shopper.groceryCart[1], 'is delicious')
 
-for(var i = 0; i < 101; i++) {
-  console.log(i % 2 === 0 ? i + ' Even' : `${i}: Odd`)
-}
+// for(var i = 0; i < 101; i++) {
+//   console.log(i % 2 === 0 ? i + ' Even' : `${i}: Odd`)
+// }
 // `${i}: Even`
+
+var officeItems = ["stapler", "monitor", "computer", "desk", "lamp", "computer", "lamp", "stapler", "computer",  "computer"]
+
+var count = 0;
+for(var i = 0; i < officeItems.length; i++) {
+  officeItems[i] === 'computer' ? count += 1 : null
+}
+// console.log(`There are ${count} computers in this array!`)
+
+var peopleWhoWantToSeeMadMaxFuryRoad = [
+  {
+    name: "Mike",
+    age: 12,
+    gender: "male"
+  },{
+    name: "Madeline",
+    age: 80,
+    gender: "female"
+  },{
+    name: "Cheryl",
+    age: 22,
+    gender: "female"
+  },{
+    name: "Sam",
+    age: 30,
+    gender: "male"
+  },{
+    name: "Suzy",
+    age: 4,
+    gender: "female"
+  }
+] 
+
+peopleWhoWantToSeeMadMaxFuryRoad.forEach(e => {
+  if(e.age >= 18 && e.gender === "female" ) {
+    console.log(`${e.name} is old enough. SHE'S good to see Mad Max Fury Road`)
+  } if(e.age >= 18 && e.gender === "male") {
+    console.log(`${e.name} is old enough. HE'S good to see Mad Max Fury Road`)
+  } if(e.age < 18 && e.gender === "female") {
+    console.log(`${e.name} is not old enough. Don't let HER in!`)
+  } else if(e.age < 18 && e.gender === "male") {
+    console.log(`${e.name} is not old enough. Don't let HIM in!`)
+  }
+})
+
