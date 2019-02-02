5d31699a6769fd0da2fbdab49a0127b9a4e1f75d
Lara Potjewyd <larapotjewyd@gmail.com>
2019-02-02 16:25:17 -0700

    finished mario pest control

--- projects/mario-pest-control/app.js
+++ projects/mario-pest-control/app.js
@@ -1,29 +1,19 @@
 var pestForm = document.pest_form;
-// var total = 0;
 
 function countPests(e) {
   e.preventDefault();
   var costPerG = Number(pestForm.goomba.value) * 5;
   var costPerB = Number(pestForm.bob_omb.value) * 7;
   var costPerC = Number(pestForm.cheep_cheep.value) * 11;
-  var total = costPerG + costPerB + costPerC;
-
-  document.getElementById("total").textContent = `${total}`;
+  var totalPrice = costPerG + costPerB + costPerC;
+  
+  document.getElementById("total").textContent = `${totalPrice} coins`;
+  document.getElementById("breakdown").innerHTML = `
+  <strong>price breakdown</strong> 
+  <div>
+    ${costPerG} for Goombas <br>
+    ${costPerB} for Bob-ombs <br>
+    ${costPerC} for Cheep-cheeps
+  </div>`;
 }
-
 document.getElementById("calculate").addEventListener('click', countPests);
-
-
-// function countGoombas(e) {
-//   e.preventDefault()
-//   total = Number(pestForm.goomba.value) * 5
-//   document.getElementById("total").textContent = `${total}`
-// }
-// document.getElementById("addGoomba").addEventListener('click', countGoombas);
-
-// function countBob(e) {
-//   e.preventDefault()
-//   var total = Number(pestForm.bob_omb.value) * 5
-//   document.getElementById("total").textContent = `${total}`
-// }
-// document.getElementById("addBob").addEventListener('click', countGoombas);
