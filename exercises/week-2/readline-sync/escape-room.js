2088209e001696b90ed1a4aaae6dd3793bca283e
Lara Potjewyd <larapotjewyd@gmail.com>
2019-02-05 13:55:25 -0700

    formatted escape room

--- exercises/week-2/readline-sync/escape-room.js
+++ exercises/week-2/readline-sync/escape-room.js
@@ -6,38 +6,45 @@ var response = ask.keyInSelect(options, '\n Yikes! You\'re locked in a room! How
 var haveKey = false;
 var trapped = true;
 
-
 while(trapped) {
   if(options[response] === 'Find the key') {
     console.clear()
     haveKey = true;
     var foundKey = ask.keyInSelect(options, '\n Yay! You have the key!');
+
       if(options[foundKey] === 'Open the door') {
         console.clear()
         console.log('\n OMG, you know how to navigate a locked door! YOU ARE FREEEEEEEEE!');
         trapped = false;
+
     } else if(options[foundKey] === 'Put hand in hole') {
         console.clear()
         console.log('\n You dead');
         trapped = false;
     }
   } 
+
   else if(options[response] === 'Open the door') {
         console.clear()
+
       if(!haveKey) {
         console.clear()
         var responseInOpenDoor = ask.keyInSelect(options, '\n Oi! Get the key first! Start again!');
+
          if(options[responseInOpenDoor] === 'Put hand in hole') {
             console.clear()
             console.log('\n You dead');
             trapped = false;
+
         } if(options[responseInOpenDoor] === 'Find the key') {
             console.clear()
             var responseInFindKey = ask.keyInSelect(options, '\n You found the key!!!');
+
             if(options[responseInFindKey] === 'Open the door') {
               console.clear()
               console.log('\n OMG, you know how to navigate a locked door! YOU ARE FREEEEEEEEE!');
               trapped = false;
+              
             } else if(options[responseInFindKey] === 'Put hand in hole') {
               console.clear()
               console.log('\n You dead');
