6639beecb912bccb884855dbe353694044adeb50
Lara Potjewyd <larapotjewyd@gmail.com>
2019-02-09 23:04:01 -0700

    working on rpg

--- projects/console-rpg/app.js
+++ projects/console-rpg/app.js
@@ -8,9 +8,11 @@ var player = {
   hasWon: false
 }
 
-var list = player.inventory.map(item => {
-  return  ` ${item}`
-})
+function listInventory() {
+  return player.inventory.map(item => {
+    return  ` ${item}`
+  })
+}
 
 function Enemy(name, attack, health, weapon) {
   this.name = name,
@@ -26,12 +28,12 @@ var attacks = {
 }
 
 var enemies = [
-  davros = new Enemy('Davros', attacks.lowAttackPoints, 100, 'Dalek Gunstick'),
-  weepingAngel = new Enemy('Weeping Angel', attacks.lowAttackPoints, 100, 'Stone Angel Hand'),
-  missy = new Enemy('Missy', attacks.medAttackPoints, 100, 'Sonic Umbrella'),
-  master = new Enemy('The Master', attacks.highAttackPoints, 100, 'Insanity'),
-  vashtaNerada = new Enemy('Vashta Nerada', attacks.highAttackPoints, 150, 'Fear of the Dark'),
-  slitheen = new Enemy('Blon Fel Fotch Pasameer-Day Slitheen', attacks.lowAttackPoints, 50, 'Huge claw and Toxic Gas')
+  davros = new Enemy('Davros', attacks.lowAttackPoints, 1000, 'Dalek Gunstick'),
+  weepingAngel = new Enemy('Weeping Angel', attacks.lowAttackPoints, 1000, 'Stone Angel Hand'),
+  missy = new Enemy('Missy', attacks.medAttackPoints, 1000, 'Sonic Umbrella'),
+  master = new Enemy('The Master', attacks.highAttackPoints, 1000, 'Insanity'),
+  vashtaNerada = new Enemy('Vashta Nerada', attacks.highAttackPoints, 1500, 'Fear of the Dark'),
+  slitheen = new Enemy('Blon Fel Fotch Pasameer-Day Slitheen', attacks.lowAttackPoints, 5000, 'Huge claw and Toxic Gas')
 ]
 
 //STORY STARTS
@@ -81,36 +83,28 @@ var choice = ask.keyIn(`\n You hear a strange wheezing whirring sound about 30 f
 player.name = ask.question(`\n It's the TARDIS! The Doctor stumbles out and begins talking at you, \n \n 'That was quite a landing! I'm in a hurry! There are aliens all over this forest and there's a band of ramblers headed in this direction! \n Quick, quick, what's your name?!' \n`)
 
 // You answer, and compliment him on his bow-tie.
-console.log(`\n '"${player.name}", eh? You're right, bow ties ARE cool. I keep telling everyone but no one listens...Well come along!' \n`)
+console.log(`\n 'Hello ${player.name}! You're right, bow ties ARE cool. I keep telling everyone but no one listens...Well come along!' \n`)
 console.clear()
 console.log(`\n As you are now the honorary Companion of The Doctor, here are the rules. \n You keep running, and fight at every opportunity. \n`)
 
 //GAME TIME!
+
 while(player.health > 0 && !player.hasWon) {
-var decisionTime = ask.keyIn(` \n I know you're not really into cardio but do you keep running with The Doctor[r]? Or check your pockets[p]? `, {limit: 'rp'})
+  var decisionTime = ask.keyIn(` \n I know you're not really into cardio but do you keep running with The Doctor[r]? Or stop to check your pockets[p]? `, {limit: 'rp'})
 
   if(decisionTime === 'r') {
     run()
 
     } else {
       console.clear()
-
-    console.log(`\n You have: ${list} to use when in a fight with an alien \n`)
+      console.log(`\n You are ${player.name}, you have: ${listInventory()} to use when in a fight with an alien, and ${player.health} health points \n`)
   }
 
 function run() {
   var random = Math.floor(Math.random() * 3)
 
   if(random === 2) {
-
-    if(enemies[0]) {
       alienEncounter()
-
-    } else {
-      console.log(`\nYAY! There are no more aliens in the woods! The approaching ramblers can ramble in peace! \n The Doctor shouts some gibberish about wibbly wobbly timey wimey stuff while he runs back to the TARDIS without a word of thanks. \n You tut and mutter to yourself as you drag your stump leg through the woods... \n `)
-      console.log(`END OF THE GAME \n Thank you for playing, ${player.name}!`)
-      player.hasWon = true
-    }
 
   } else {
     console.log(`\n The Doctor shouts at you to keep up the pace. \n`)
@@ -123,63 +117,86 @@ function alienEncounter() {
   var action = ask.keyIn(`\n We finally stopped running! But yikes, there's an aggressive looking beastie! You have ${player.health} health points left. Run awaaay[a] or Fight[f]?!`, {limit: 'af'})
 
     if(action === 'f') {
-      var alien = alienSelect()
-      console.log(`\n ${enemies[alien].name} is gearing up for a fight!`)
-      fiiight(enemies[alien])
+      var selectedAlien = Math.floor(Math.random() * enemies.length)      
+        console.log(`\n ${enemies[selectedAlien].name} is gearing up for a fight!`)
+        fiiight(enemies[selectedAlien])
 
     } else {
-      run()
+        var escapeOdds = Math.floor(Math.random() * 2)
+        if(escapeOdds === 1) {
+          run()
+        } else {
+          player.health -= 20
+          console.log(`\n You didn't escape fast enough so you lost 20 health points. Boo.`)
+        }
     }
 }
 
-function alienSelect() {
-  var selectedAlien = Math.floor(Math.random() * enemies.length)
-  return selectedAlien
-}
-
 function fiiight(alien) {
-  var randomNum = Math.floor(Math.random() * 6)
-  if(randomNum === 2) {
-    missedThatAlien()
 
-  } else if(randomNum === 3) {
-    console.log(`\n That frying pan came in useful! \n Who knew Teflon also repels alien attacks?! \n`)
+  while(alien.health > 0) {
 
-  } else {
-    player.health -= alien.attack
-    alien.health -=player.attack
+    var randomNum = Math.floor(Math.random() * 5)
 
-      if(alien.health <= 0) {
-        player.inventory.push(alien.weapon)
-        var index = enemies.indexOf(alien)
-        enemies.splice(index, 1)
-        console.log(`\n ${alien.name} is now dead and you have their ${alien.weapon}!`)
+    if(randomNum === 2) {
+      console.log(`\n Woops! You missed! \n`)  
+  
+    } else if(randomNum === 3) {
+        console.log(`\n That frying pan came in useful! \n Who knew Teflon also repels alien attacks?! \n`)
 
-      } else {
-        console.log(`\n ${alien.name} has ${alien.health} health points left`)
-      }
+    } else {
+      var attack = ask.keyIn(`\n ATTACK![y] - OR - RUN AWAY![n]`, {limit: 'yn'})
+        if(attack === 'y') {
+          player.health -= alien.attack
+          alien.health -=player.attack
+    
+            if(alien.health <= 0) {
+              player.health += 20
+              player.inventory.push(alien.weapon)
+              var index = enemies.indexOf(alien)
+              enemies.splice(index, 1)
+              console.log(`\n ${alien.name} is now dead and you have their ${alien.weapon}, and gained 20 health points from the thrill of the kill!`)
+    
+            } else {
+              console.log(`\n You managed to take ${player.attack} points from ${alien.name}, who now has ${alien.health} health points left`)
+            }
+        } else {
+          var escapeOdds = Math.floor(Math.random() * 2)          
+          if(escapeOdds === 1) {
+            run()
+          } else {
+            player.health -= 20
+            console.log(`\n You didn't escape fast enough so you lost 20 health points. Boo.`)
+        }
+    }
+  }
 
       if(player.health <= 0) {
         console.log(` \n Good job for failing The Doctor by dying. \n GAME OVER!`)
 
       } else {
-        console.log(`\n That was rough, your health is now ${player.health} \n Keep an eye on that \n `)
+        console.log(`\n That was rough, you took ${alien.attack} damage. Your health is now ${player.health} \n Keep an eye on that \n `)
       }
     
   }
-}
+  if(!enemies[0]) {
+    console.clear()
 
-  function missedThatAlien() {
-    console.log(`\n Woops! You missed! Run awaaaay! \n`)
+    console.log(`\n YAY! There are no more aliens in the woods! The approaching ramblers can ramble in peace! \n The Doctor shouts some gibberish about wibbly wobbly timey wimey stuff while he runs back to the TARDIS without a word of thanks. \n You tut and mutter to yourself as you drag your stump leg through the woods... \n `)
+    console.log(`\n END OF THE GAME \n Thank you for playing, ${player.name}!`)
+    player.hasWon = true
   }
+}
 
 };
 
-var finalRequest = ask.keyIn(`\n Would you like to see your end game stats[y]? Or no[n]?`, {limit: 'yn'})
-if(finalRequest === 'y') {
-  var enemyList = enemies.map(enemy => {
-    return ` ${enemy.name}`
-  })
-  console.log(`You were ${player.name}, you had ${player.health} health points, and these items: ${list}.
-  You had these enemies left to defeat: ${enemyList}.`)
+  var finalRequest = ask.keyIn(`\n Would you like to see your end game stats[y]? Or no[n]?`, {limit: 'yn'})
+  if(finalRequest === 'y') {
+    var enemyList = enemies.map(enemy => {
+      return ` ${enemy.name}`
+    })
+
+  console.log(`\n You were ${player.name}, you had ${player.health} health points, and these items: ${listInventory()}.
+  You had these enemies left to defeat: ${!enemyList[0] ? `none, you killed them all!` : enemyList}`)
+
 }
