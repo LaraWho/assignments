40680c97d3a192e4f60e8910f5864bc90737a11b
Lara Potjewyd <larapotjewyd@gmail.com>
2019-02-10 14:34:04 -0700

    added dynamic point taking

--- projects/console-rpg/app.js
+++ projects/console-rpg/app.js
@@ -3,7 +3,9 @@ var ask = require("readline-sync");
 var player = {
   name: '',
   health: 300,
-  attack: Math.floor(Math.random() * 50) + 30,
+  attack: function() {
+    return Math.floor(Math.random() * 50) + 30
+  },
   inventory: ['dirty hanky', 'frying pan'],
   hasWon: false
 }
@@ -21,19 +23,28 @@ function Enemy(name, attack, health, weapon) {
   this.weapon = weapon
 }
 
-var attacks = {
-  lowAttackPoints: Math.floor(Math.random() * 20) + 10,
-  medAttackPoints: Math.floor(Math.random() * 30) + 20,
-  highAttackPoints: Math.floor(Math.random() * 40) + 30,
+Enemy.prototype.attackPoints = function() {
+  var attacks = {
+    low() {
+      return Math.floor(Math.random() * 20) + 10
+    },
+    medium() {
+      return Math.floor(Math.random() * 30) + 20
+    },
+    high() {
+      return Math.floor(Math.random() * 40) + 30
+    } 
+  }
+  return attacks[this.attack]()
 }
 
 var enemies = [
-  davros = new Enemy('Davros', attacks.lowAttackPoints, 175, 'Dalek Gunstick'),
-  weepingAngel = new Enemy('Weeping Angel', attacks.lowAttackPoints, 100, 'Stone Angel Hand'),
-  missy = new Enemy('Missy', attacks.medAttackPoints, 100, 'Sonic Umbrella'),
-  master = new Enemy('The Master', attacks.highAttackPoints, 150, 'Insanity'),
-  vashtaNerada = new Enemy('Vashta Nerada', attacks.highAttackPoints, 150, 'Fear of the Dark'),
-  slitheen = new Enemy('Blon Fel Fotch Pasameer-Day Slitheen', attacks.lowAttackPoints, 50, 'Huge claw and Toxic Gas')
+  davros = new Enemy('Davros', 'low', 175, 'Dalek Gunstick'),
+  weepingAngel = new Enemy('Weeping Angel', 'low', 100, 'Stone Angel Hand'),
+  missy = new Enemy('Missy', 'medium', 100, 'Sonic Umbrella'),
+  master = new Enemy('The Master', 'high', 150, 'Insanity'),
+  vashtaNerada = new Enemy('Vashta Nerada', 'high', 150, 'Fear of the Dark'),
+  slitheen = new Enemy('Blon Fel Fotch Pasameer-Day Slitheen', 'low', 50, 'Huge claw and Toxic Gas')
 ]
 
 //STORY STARTS
@@ -84,12 +95,14 @@ player.name = ask.question(`\n It's the TARDIS! The Doctor stumbles out and begi
 
 // You answer, and compliment him on his bow-tie.
 console.log(`\n 'Hello ${player.name}! You're right, bow ties ARE cool. I keep telling everyone but no one listens...Well come along!' \n`)
-console.clear()
-console.log(`\n As you are now the honorary Companion of The Doctor, here are the rules. \n You keep running, and fight at every opportunity. \n`)
+ask.keyInPause(`\n As you are now the honorary Companion of The Doctor, here are the rules. \n You keep running, and fight at every opportunity. \n`)
 
 //GAME TIME!
 
 while(player.health > 0 && !player.hasWon) {
+
+  // ask.keyInPause(`\n You're in the woods alien hunting with The Doctor, `)
+
   var decisionTime = ask.keyIn(` \n I know you're not really into cardio but do you keep running with The Doctor[r]? Or stop to check your pockets[p]? `, {limit: 'rp'})
 
   if(decisionTime === 'r') {
@@ -128,65 +141,80 @@ function alienEncounter() {
         } else {
           player.health -= 20
           console.log(`\n You didn't escape fast enough so you lost 20 health points. Boo.`)
+          run()
         }
     }
 }
 
-function fiiight(alien) {
 
-  while(alien.health > 0) {
+function fiiight(alien) {
+  var runAway = false
+
+  while(alien.health > 0 && !runAway) {
 
     var randomNum = Math.floor(Math.random() * 5)
     var attack = ask.keyIn(`\n ATTACK![y] - OR - RUN AWAY![n]`, {limit: 'yn'})
       if(attack === 'y') {
 
-        if(randomNum === 2) {
+        if(randomNum === 0) {
           console.log(`\n Woops! You missed! \n`)  
       
-        } else if(randomNum === 3) {
+        } else if(randomNum === 1) {
             console.log(`\n That frying pan came in useful! \n Who knew Teflon also repels alien attacks?! \n`)
 
         } else {
-          player.health -= alien.attack
-          alien.health -=player.attack
-    
-            if(alien.health <= 0) {
-              player.health += 20
-              player.inventory.push(alien.weapon)
-              var index = enemies.indexOf(alien)
-              enemies.splice(index, 1)
-              console.log(`\n ${alien.name} is now dead and you have their ${alien.weapon}, and gained 20 health points from the thrill of the kill!`)
-    
-            } else {
-              console.log(`\n You managed to take ${player.attack} points from ${alien.name}, who now has ${alien.health} health points left`)
-            } 
+          var alienAttack = alien.attackPoints()
+          var playerDamage = player.attack()
+          player.health -= alienAttack
+
+          if(player.health <= 0) {
+            console.clear()
+            console.log(` \n Good job for failing The Doctor by dying. \n GAME OVER!`)
+  
+          } else {
+            console.clear()
+            console.log(`\n You managed to take ${playerDamage} points from ${alien.name}, who now has ${alien.health} health points left \n`)
+        }
+            alien.health -= playerDamage
+            ask.keyInPause(`\n Are you ready for the retaliation??`)
             
-              if(player.health <= 0) {
-                console.log(` \n Good job for failing The Doctor by dying. \n GAME OVER!`)
-      
-            } else {
-              console.log(`\n That was rough, you took ${alien.attack} damage. Your health is now ${player.health} \n Keep an eye on that \n `)
-            }
+            console.log(`\n Oh no! ${alien.name} really did a number on you!\n`)
+            console.log(`\n You took ${alienAttack} damage. Your health is now ${player.health} \n Keep an eye on that \n `)
+            
   }
         } else if(attack === 'n') {
+          console.clear()
           var escapeOdds = Math.floor(Math.random() * 2)          
           if(escapeOdds === 1) {
+            runAway = true
             run()
           } else {
-            player.health -= 20
-            console.log(`\n You didn't escape fast enough so you lost 20 health points. Boo.`)
+            var runawayDamage = alien.attackPoints()
+            player.health -= runawayDamage
+            console.log(`\n You didn't escape fast enough so you lost ${runawayDamage} health points. Boo.`)
+            runAway = true
             run()
         }
     }
     
   }
-  if(!enemies[0]) {
-    console.clear()
+      if(alien.health <= 0) {
+        var heroRecoup = alien.attackPoints()
+        player.health += heroRecoup
 
-    console.log(`\n YAY! There are no more aliens in the woods! The approaching ramblers can ramble in peace! \n The Doctor shouts some gibberish about wibbly wobbly timey wimey stuff while he runs back to the TARDIS without a word of thanks. \n You tut and mutter to yourself as you drag your stump leg through the woods... \n `)
-    console.log(`\n END OF THE GAME \n Thank you for playing, ${player.name}!`)
-    player.hasWon = true
-  }
+        player.inventory.push(alien.weapon)
+        var index = enemies.indexOf(alien)
+        enemies.splice(index, 1)
+        console.clear()
+        console.log(`\n ${alien.name} is now dead and you have their ${alien.weapon}, and gained ${heroRecoup} health points from the thrill of the kill!`)
+      }
+      
+      if(!enemies[0]) {
+        console.clear()
+        console.log(`\n YAY! There are no more aliens in the woods! The approaching ramblers can ramble in peace! \n The Doctor shouts some gibberish about wibbly wobbly timey wimey stuff while he runs back to the TARDIS without a word of thanks. \n You tut and mutter to yourself as you drag your stump leg through the woods... \n `)
+        console.log(`\n END OF THE GAME \n Thank you for playing, ${player.name}!`)
+        player.hasWon = true
+      }
 }
 
 };
