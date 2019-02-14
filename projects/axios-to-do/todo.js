a84d055677a28c6b9df3b19262abd00c9dbce668
Lara Potjewyd <larapotjewyd@gmail.com>
2019-02-13 17:02:08 -0700

    working on axios to do

--- projects/axios-to-do/todo.js
+++ projects/axios-to-do/todo.js
@@ -1,8 +1,7 @@
-var todoItem = document.getElementById("list-box")
+var listContainer = document.getElementById("list-box")
 var addNew = document.todo_form
 var todoList = document.list_box
 var myList = []
-
 
 axios.get("https://api.vschool.io/lara/todo").then((response) => {
   response.data.map((item) => {
@@ -21,28 +20,32 @@ renderList = (list) => {
     var para = document.createElement("p")
     var input = document.createElement("input")
     var deleteBtn = document.createElement("button")
+    var editSaveBtn = document.createElement("button")
     deleteBtn.textContent = "delete"
+    editSaveBtn.textContent = "edit"
+
     input.type = "checkbox"
     input.className = "checkbox"
     input.name = "item"
-    input.value = item._id
+    input.value = item.completed
     todoBox.className = "todo-item"
     
     input.addEventListener("click", updateComplete)
-      // e.target.parentNode.childNodes[2].classList.toggle("checked")
-  
 
     deleteBtn.addEventListener("click", deleteItem)
     todoBox.id = item._id
 
+    editSaveBtn.addEventListener("click", editSaveFn)
+
     title.textContent = `${item.title}`
     para.textContent = ` ${item.description}`
 
-    todoItem.appendChild(todoBox)
+    listContainer.prepend(todoBox)
     todoBox.appendChild(title)
     todoBox.appendChild(input)
     todoBox.appendChild(para)
     todoBox.appendChild(deleteBtn)
+    todoBox.appendChild(editSaveBtn)
 
           if(item.completed) {
             para.classList.add("checked")
@@ -56,8 +59,12 @@ renderList = (list) => {
             image.className = "todo-img"
             todoBox.appendChild(image)
           }
-    
   })
+}
+
+editSaveFn = (e) => {
+  console.log(e.target.value)
+  e.preventDefault()
 }
 
 deleteItem = (e) => {
@@ -73,13 +80,20 @@ postNewToDo = (e) => {
   var newTitle = addNew.todo_title.value
   var newDesc = addNew.todo_desc.value
   var newImg = addNew.todo_img.value
-
+  
   axios.post("https://api.vschool.io/lara/todo", {
     title: newTitle,
     description: newDesc,
     imgUrl: newImg
+    
   }).then((response) => {
     myList.push(response.data)
+    renderList([response.data])
+
+    addNew.todo_title.value = ''
+    addNew.todo_desc.value = ''
+    addNew.todo_img.value = ''
+
   }).catch((error) => {
     console.log(error)
   })
@@ -90,10 +104,14 @@ addNew.addEventListener("submit", postNewToDo)
 
 updateComplete = (e) => {
   e.target.parentNode.childNodes[2].classList.toggle("checked")
-  var bool = false
-
+    var bool = false
+      if(e.target.value === "true") {
+        bool = false
+      } else {
+        bool = true
+      }
   axios.put(`https://api.vschool.io/lara/todo/${e.target.parentElement.id}`, {
-    completed: !bool
+    completed: bool
   }).then((response) => {
     console.log(response.data.completed)
   }).catch((error) => {
