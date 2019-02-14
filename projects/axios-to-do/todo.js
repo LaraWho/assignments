e6ffb410ec5a2ba68742818b48375974ef073366
Lara Potjewyd <larapotjewyd@gmail.com>
2019-02-14 14:55:26 -0700

    finished todo project

--- projects/axios-to-do/todo.js
+++ projects/axios-to-do/todo.js
@@ -23,6 +23,7 @@ renderList = (list) => {
     var deleteBtn = document.createElement("button")
     var editSaveBtn = document.createElement("button")
     deleteBtn.textContent = "delete"
+    deleteBtn.classList = "delete"
     editSaveBtn.textContent = "edit"
     editSaveBtn.classList = "edit"
     title.type = "text"
@@ -55,43 +56,42 @@ renderList = (list) => {
     todoBox.appendChild(title)
     todoBox.appendChild(input)
     todoBox.appendChild(para)
+      if(item.imgUrl) {
+        var image = document.createElement("img")
+        var imgUrl = item.imgUrl
+        image.src = imgUrl
+        image.className = "todo-img"
+        todoBox.appendChild(image)
+      } 
+
     todoBox.appendChild(deleteBtn)
     todoBox.appendChild(editSaveBtn)
 
           if(item.completed) {
             para.classList.add("checked")
+            title.classList.add("checked")
             input.checked = true
           }
 
-          if(item.imgUrl) {
-            var image = document.createElement("img")
-            var imgUrl = item.imgUrl
-            image.src = imgUrl
-            image.className = "todo-img"
-            todoBox.appendChild(image)
-          }
   })
 }
 
 
 editSaveFn = (e) => {
+  console.log(e.target.parentNode.childNodes)
   e.preventDefault()
-  // var updatedObj = {
-  //   newTitle: addNew.title.value,
-  //   newDesc: addNew.desc.value,
-  // }
     e.target.parentNode.childNodes[0].toggleAttribute("disabled")
     e.target.parentNode.childNodes[2].toggleAttribute("disabled")
     e.target.parentNode.childNodes[0].classList.toggle("show-input")
     e.target.parentNode.childNodes[2].classList.toggle("show-input")
         
-    e.target.parentNode.childNodes[4].textContent === "edit" ? e.target.parentNode.childNodes[4].textContent = "save" : e.target.parentNode.childNodes[4].textContent = "edit"
+    e.target.parentNode.childNodes[5].textContent === "edit" ? e.target.parentNode.childNodes[5].textContent = "save" : e.target.parentNode.childNodes[5].textContent = "edit"
     
-  axios.put(`https://api.vschool.io/lara/todo/${e.target.parentElement.id}`).then((response) => {
-    console.log(todoList.title)
-    console.log(e.target.parentNode.childNodes)
-    e.target.parentNode.childNodes[0].value = updatedObj.newTitle
-    e.target.parentNode.childNodes[2].value = updatedObj.newDesc
+  axios.put(`https://api.vschool.io/lara/todo/${e.target.parentElement.id}`, {
+    title: e.target.parentNode.childNodes[0].value,
+    description: e.target.parentNode.childNodes[2].value
+  }).then((response) => {
+    console.log(response.data)
     
   }).catch((error) => {
     console.log(error)
@@ -111,8 +111,14 @@ deleteItem = (e) => {
 postNewToDo = (e) => {
   var newTitle = addNew.todo_title.value
   var newDesc = addNew.todo_desc.value
-  var newImg = addNew.todo_img.value
-  
+  console.log(addNew.todo_img.value === "")
+
+  if(addNew.todo_img.value === "") {
+    var newImg = "https://akns-images.eonline.com/eol_images/Entire_Site/2018120/rs_600x600-180220042623-600.benedict-cumberbatch-omaze.22018.jpg?fit=around|700:700&crop=700:700;center,top&output-quality=90"
+  } else {
+    var newImg = addNew.todo_img.value
+  }
+
   axios.post("https://api.vschool.io/lara/todo", {
     title: newTitle,
     description: newDesc,
@@ -136,6 +142,7 @@ addNew.addEventListener("submit", postNewToDo)
 
 updateComplete = (e) => {
   e.target.parentNode.childNodes[2].classList.toggle("checked")
+  e.target.parentNode.childNodes[0].classList.toggle("checked")
     var bool = false
       if(e.target.value === "true") {
         bool = false
