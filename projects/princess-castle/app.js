49c2505981e94bad7bf4161b0e37f40e7def39b1
Lara Potjewyd <larapotjewyd@gmail.com>
2019-02-18 12:07:27 -0700

    adding extra media query

---
+++ projects/princess-castle/app.js
@@ -0,0 +1,14 @@
+class Player {
+  constructor(name, status) {
+    this.name = name,
+    this.totalCoins = 0,
+    this.status = status,
+    this.hasStar = false
+  }
+  setName() {
+    this.namePicked = this.name
+  }
+  gotHit() {
+    this.status = 
+  }
+}
