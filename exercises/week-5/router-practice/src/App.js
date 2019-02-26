362f44dffd9249d542952b08c8b58933509bbc54
Lara Potjewyd <larapotjewyd@gmail.com>
2019-02-26 16:28:01 -0700

    working on stuff

---
+++ exercises/week-5/router-practice/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Link, Switch } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+        
+
+        
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ exercises/week-5/router-practice/src/Services.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Services extends Component {
+  render() {
+    return(
+      <div>
+  
+      </div>
+    )
+  }
+}
+export default Services;
