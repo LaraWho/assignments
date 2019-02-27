a05e6ae8dd86dbf23d98ef737418c97b6fc0d110
Lara Potjewyd <larapotjewyd@gmail.com>
2019-02-27 12:35:56 -0700

    finished name badge

---
+++ exercises/week-5/name-badge/src/Form.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+import NameBadge from './NameBadge';
+
+class Form extends Component {
+  constructor(props) {
+    super(props)
+      this.state = {
+        inputs: {
+          firstName: '',
+          lastName: '',
+          email: '',
+          phone: '',
+          birthPlace: '',
+          favFood: '',
+          info: ''
+        },
+        people: [
+          {
+            firstName: 'Lara',
+            lastName: 'Potjewyd',
+            email: 'larapotjewyd@gmail.com',
+            phone: '3856664892',
+            birthPlace: 'Salt Lake City',
+            favFood: 'Pear and Pecorino ravioli',
+            info: 'My writing gets worse and worse. Dear, dear, dear, dear, dear. Rest is for the weary, sleep is for the dead. I feel like a hungry man eager for the feast!'
+          },
+          {
+            firstName: 'Lara',
+            lastName: 'Potjewyd',
+            email: 'larapotjewyd@gmail.com',
+            phone: '3856664892',
+            birthPlace: 'Salt Lake City',
+            favFood: 'Pear and Pecorino ravioli',
+            info: 'My writing gets worse and worse. Dear, dear, dear, dear, dear. Rest is for the weary, sleep is for the dead. I feel like a hungry man eager for the feast!'
+          },
+          {
+            firstName: 'Lara',
+            lastName: 'Potjewyd',
+            email: 'larapotjewyd@gmail.com',
+            phone: '3856664892',
+            birthPlace: 'Salt Lake City',
+            favFood: 'Pear and Pecorino ravioli',
+            info: 'My writing gets worse and worse. Dear, dear, dear, dear, dear. Rest is for the weary, sleep is for the dead. I feel like a hungry man eager for the feast!'
+          }
+        ]
+      }
+    }
+
+
+  handleChange = (e) => {
+    const { name, value } = e.target
+    this.setState(prevState => {
+      return({
+        inputs: {
+          ...prevState.inputs,
+          [name]: value
+        }
+      })
+    })
+  }
+
+  addPerson = (e) => {
+    e.preventDefault()
+    this.setState((prevState) => {
+      return ({
+        people: [prevState.inputs, ...prevState.people],
+        inputs: {
+          firstName: '',
+          lastName: '',
+          email: '',
+          phone: '',
+          birthPlace: '',
+          favFood: '',
+          info: ''
+        }
+      })
+    })
+  }
+
+  render() {
+    let { firstName, lastName, email, phone, birthPlace, favFood, info} = this.state.inputs
+
+    let mappedPeople = this.state.people.map((person, i) => {
+        let backgroundColor;
+        i % 2 === 0 ? backgroundColor = "#e22828" : backgroundColor = "#1b1b8c"
+        return <NameBadge badge={person} key={i} backgroundColor={backgroundColor}/> 
+    })
+    
+    return(
+      <div>
+        <form onSubmit={this.addPerson} name="personForm">
+          <input type="text" name="firstName" value={firstName} onChange={this.handleChange} placeholder="First Name" required />
+
+          <input type="text" name="lastName" value={lastName} onChange={this.handleChange} placeholder="last Name" required />
+
+          <input type="text" name="email" value={email} onChange={this.handleChange}placeholder="Email" required />
+
+          <input type="phone" name="phone" value={phone} onChange={this.handleChange} placeholder="Phone" required />
+
+          <input type="text" name="birthPlace" value={birthPlace} onChange={this.handleChange} placeholder="Place of Birth" required />
+
+          <input type="text" name="favFood" value={favFood} onChange={this.handleChange} placeholder="Favourite Food" required />
+
+          <textarea name="info" value={info} onChange={this.handleChange} placeholder="Tell us about yourself..." required cols="10" rows="5" ></textarea>
+
+          <button>Submit</button>
+        </form> 
+          {mappedPeople}
+      </div>    
+    )
+  }
+}
+export default Form;
--- exercises/week-5/name-badge/src/NameBadge.js
+++ exercises/week-5/name-badge/src/NameBadge.js
@@ -1,87 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class NameBadge extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      firstName: '',
-      lastName: '',
-      email: '',
-      phone: '',
-      birthPlace: '',
-      favFood: '',
-      info: '',
-      people: []
-    }
-  }
+const NameBadge = (props) => {
 
-  addPerson = (e) => {
-    e.preventDefault()
-    // let { phone } = this.state.phone
-    // if(typeof phone === "string") {
-    //   return Number(phone)
-    // }
-    // console.log(typeof this.state.phone)
-    this.setState((prevState) => {
-      let { firstName, lastName, email, phone, birthPlace, favFood, info } = prevState
-      return ({
-        people: [...prevState.people, {firstName, lastName, email, phone, birthPlace, favFood, info}],
-        firstName: '',
-        lastName: '',
-        email: '',
-        phone: '',
-        birthPlace: '',
-        favFood: '',
-        info: ''
-      })
-    })
-  }
-
-  handleChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value
-    })
-  }
-
-  render() {
-    let mappedPeople = this.state.people.map((person, i) => {
-      return(
-        <div key={i}>
-          <h1>Badge:</h1>
-          <p>Name: {person.firstName} {person.lastName}</p>
-          <p>Place of Birth: {person.birthPlace}</p>
-          <p>Email: {person.email}</p>
-          <p>Phone: {person.phone}</p>
-          <p>Favourite Food: {person.favFood}</p>
-          <p>{person.info}</p>
-        </div>
-      )
-    })
-
-    return(
-    <div>
-      <form onSubmit={this.addPerson} name="personForm">
-        <input type="text" name="firstName" value={this.state.firstName} onChange={this.handleChange} placeholder="First Name" id="" required/>
-
-        <input type="text" name="lastName" value={this.state.lastName} onChange={this.handleChange} placeholder="last Name" id="" required/>
-
-        <input type="text" name="email" value={this.state.email} onChange={this.handleChange}placeholder="Email" id="" required/>
-
-        <input type="text" name="phone" value={this.state.phone} onChange={this.handleChange} placeholder="Phone" id="" required/>
-
-        <input type="text" name="birthPlace" value={this.state.birthPlace} onChange={this.handleChange} placeholder="Place of Birth" id="" required/>
-
-        <input type="text" name="favFood" value={this.state.favFood} onChange={this.handleChange} placeholder="Favourite Food" id="" required/>
-
-        <textarea name="info" value={this.state.info} onChange={this.handleChange} placeholder="Tell us about yourself..." id="" cols="30" rows="10" required></textarea>
-
-        <button>Submit</button>
-      </form> 
-
-        {mappedPeople}
-    </div>
+  console.log(props.backgroundColor)
+  let { firstName, lastName, email, phone, birthPlace, favFood, info } = props.badge
+  return(
+      <div className="badge">
+          <header style={{backgroundColor: props.backgroundColor}}>Badge:</header>
+          <p className="name">Name: {firstName} {lastName}</p>
+          <p className="birth">Place of Birth: {birthPlace}</p>
+          <p className="email">Email: {email}</p>
+          <p className="phone">Phone: {phone}</p>
+          <p className="food">Favourite Food: {favFood}</p>
+          <p className="info">{info}</p>
+      </div>
     )
   }
-}
-
 export default NameBadge;
